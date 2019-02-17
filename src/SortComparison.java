43b86dbdb03424dadb527bbaddaf7697541227d4
Ciara Gilsenan <ciaragil98@gmail.com>
2019-02-17 17:26:22 +0000

    First commit

---
+++ src/SortComparison.java
@@ -0,0 +1,114 @@
+import java.util.Arrays;
+
+// -------------------------------------------------------------------------
+
+/**
+ *  This class contains static methods that implementing sorting of an array of numbers
+ *  using different sort algorithms.
+ *
+ *  @author
+ *  @version HT 2019
+ */
+
+ class SortComparison {
+
+    /**
+     * Sorts an array of doubles using InsertionSort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order.
+     *
+     */
+    static double [] insertionSort (double a[]){
+    	return null;
+
+        //todo: implement the sort
+    }//end insertionsort
+
+    /**
+     * Sorts an array of doubles using Quick Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order
+     *
+     */
+    static double [] quickSort (double a[]){
+    	return null;
+	
+		 //todo: implement the sort
+
+    }//end quicksort
+
+    /**
+     * Sorts an array of doubles using Merge Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order
+     *
+     */
+    /**
+     * Sorts an array of doubles using iterative implementation of Merge Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     *
+     * @param a: An unsorted array of doubles.
+     * @return after the method returns, the array must be in ascending sorted order.
+     */
+
+    static double[] mergeSortIterative (double a[]) {
+    	return null;
+
+		 //todo: implement the sort
+	
+    }//end mergesortIterative
+    
+    
+    
+    /**
+     * Sorts an array of doubles using recursive implementation of Merge Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     *
+     * @param a: An unsorted array of doubles.
+     * @return after the method returns, the array must be in ascending sorted order.
+     */
+    static double[] mergeSortRecursive (double a[]) {
+    	return null;
+    	
+
+    	//todo: implement the sort
+	
+   }//end mergeSortRecursive
+    	
+    
+    /**
+     * Sorts an array of doubles using Selection Sort.
+     * This method is static, thus it can be called as SortComparison.sort(a)
+     * @param a: An unsorted array of doubles.
+     * @return array sorted in ascending order
+     *
+     */
+    static double [] selectionSort (double a[]){
+    	for(int i = 0; i < a.length; i++) {
+    		int min = i;
+    		for(int j = i+1; j < a.length; j++) {
+    			if(a[j] < a[min])
+    				min = j;
+    		}
+    		//Does Swap
+    		double temp = a[min];
+    		a[min] = a[i];
+    		a[i] = temp;
+    	}
+    	
+    	return a;
+    }
+
+  
+    public static void main(String[] args) {
+    	
+    	double[] test = new double[] {4, 2, 6, 7, 9, 1};
+    	System.out.println("Before sort: " + Arrays.toString(test));
+    	System.out.println("After sort: " + Arrays.toString(selectionSort(test)));
+
+    }
+
+ }
